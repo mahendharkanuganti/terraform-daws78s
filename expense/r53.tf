5f949bc809c4d891a3366cf041f4ff201d4bf52b
mahidarling <kanuganti98@gmail.com>
2024-06-04 11:15:19 +0530

    sg module developemt

--- expense/r53.tf
+++ expense/r53.tf
@@ -2,11 +2,17 @@
 resource "aws_route53_record" "expense" {
     count = length(var.instance_names)
     zone_id = var.zone_id
-    #name = var.instance_names[count.index] == "frontend" ? var.domain_name : "${var.instance_names[count.index]}.${var.domain_name}"
-    name    = local.record_name
+    name = var.instance_names[count.index] == "frontend" ? var.domain_name : "${var.instance_names[count.index]}.${var.domain_name}"
+    #name    = local.record_name
     type    = "A"
     ttl     = 1
-    records = local.record_value
-    #records = var.instance_names[count.index] == "frontend" ? [aws_instance.expense[count.index].public_ip] : [aws_instance.expense[count.index].private_ip]
+    #records = local.record_value
+    records = var.instance_names[count.index] == "frontend" ? [aws_instance.expense[count.index].public_ip] : [aws_instance.expense[count.index].private_ip]
     allow_overwrite = true
 }
+
+
+
+# Count and count.index will work only when the count module is set in local. So  that meant Count and count.index will not work on locals
+
+# The "count" object can only be used in "module", "resource", and "data" blocks, and only when the "count" argument is set.
